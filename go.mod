c486a34f62755b74e2048a4d4e1eb0be8459ba69
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-08-24 13:44:35 -0300

    initialized project structure and adds user entity implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sancheschris/ecommerce-api
+
+go 1.24.2
---
+++ internal/entity/user.go
@@ -0,0 +1,38 @@
+package entity
+
+import "errors"
+
+var (
+	ErrNameIsRequired = errors.New("name is required")
+	ErrEmailIsRequired = errors.New("email is required")
+)
+
+type User struct {
+	ID int64 `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	newUser := &User{
+		Name: name,
+		Email: email,
+		Password: password,
+	}
+	err := newUser.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return newUser, nil
+}
+
+func (u *User) Validate() error {
+	if u.Name == "" {
+		return ErrNameIsRequired
+	}
+	if u.Email == "" {
+		return ErrEmailIsRequired
+	}
+	return nil
+}
