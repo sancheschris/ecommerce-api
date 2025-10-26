54394e449438c8ef1dba5efae4be93f1d5323331
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-10-26 13:36:09 -0300

    add mock payment repo methods

--- internal/service/payment_mock.go
+++ internal/service/payment_mock.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/sancheschris/ecommerce-api/internal/model"
 	"github.com/stretchr/testify/mock"
 	stripe "github.com/stripe/stripe-go/v76"
 )
@@ -33,5 +34,37 @@ type MockPaymentRepo struct {
 	mock.Mock
 }
 
+func (m *MockPaymentRepo) Create(payment *model.Payment) error {
+	args := m.Called(payment)
+	return args.Error(0)
+}
 
+func (m *MockPaymentRepo) GetByID(id int) (*model.Payment, error) {
+	args := m.Called(id)
+	return args.Get(0).(*model.Payment), args.Error(1)
+}
 
+func (m *MockPaymentRepo) Update(payment *model.Payment) error {
+	args := m.Called(payment)
+	return args.Error(0)
+}
+
+func (m *MockPaymentRepo) Delete(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockPaymentRepo) GetByOrderID(orderID int) (*model.Payment, error) {
+    args := m.Called(orderID)
+    return args.Get(0).(*model.Payment), args.Error(1)
+}
+
+func (m *MockPaymentRepo) GetByUserID(userID int) (*model.Payment, error) {
+    args := m.Called(userID)
+    return args.Get(0).(*model.Payment), args.Error(1)
+}
+
+func (m *MockPaymentRepo) GetByStatus(status string) ([]*model.Payment, error) {
+    args := m.Called(status)
+    return args.Get(0).([]*model.Payment), args.Error(1)
+}
